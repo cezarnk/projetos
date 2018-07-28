f0b3997088b25b0025b12ff1b7ce8a89e5a013b8
cezarnk <cezar.nk@gmail.com>
2018-07-28 18:16:47 -0300

    incluido documento com a avaliação da matéria Java JSE

--- PosEAD/Disciplinas/7 - Java Jse - Programação Avançada/threadExemplo/src/threadExemplo/ExemploThread.java
+++ PosEAD/Disciplinas/7 - Java Jse - Programação Avançada/threadExemplo/src/threadExemplo/ExemploThread.java
@@ -2,13 +2,14 @@ package threadExemplo;
 
 public class ExemploThread extends Thread{
 	public void run(){
-		try{
+		try {
 			for (int i=0;i<20;i++){
 				System.out.println("y");
 				Thread.sleep(200);
 			}
-		}catch (Exception e){
-			System.out.println("Houve um erro!");
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
+		
 	}
 }
