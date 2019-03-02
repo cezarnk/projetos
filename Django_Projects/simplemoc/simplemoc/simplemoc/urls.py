0a37b73ef06d94f009257a382bd04bf12a8094ab
cezar.nk <cezar.nk@gmail.com>
2019-03-02 00:04:47 -0300

    criado banco de dados sqlite do projeto django

--- Django_Projects/simplemoc/simplemoc/simplemoc/urls.py
+++ Django_Projects/simplemoc/simplemoc/simplemoc/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'simplemoc.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'simplemoc.core.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
