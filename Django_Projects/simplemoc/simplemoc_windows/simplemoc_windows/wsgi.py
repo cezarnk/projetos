597f688857435bd7dcb9b3a1ad9a0e1753dbcf34
cezar.nk <cezar.nk@gmail.com>
2019-03-02 00:06:56 -0300

    incluido arquivos do projeto django de windows

--- Django_Projects/simplemoc/simplemoc_windows/simplemoc_windows/wsgi.py
+++ Django_Projects/simplemoc/simplemoc_windows/simplemoc_windows/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for simplemoc_windows project.
+WSGI config for simplemooc project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplemoc_windows.settings')
-
 application = get_wsgi_application()
