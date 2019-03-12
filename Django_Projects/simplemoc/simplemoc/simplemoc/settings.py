209b209621f4a4b309add1195540a085c5507a81
cezar.nk <cezar.nk@gmail.com>
2019-03-12 02:02:09 -0300

    incluido lista de programas

--- Django_Projects/simplemoc/simplemoc/simplemoc/settings.py
+++ Django_Projects/simplemoc/simplemoc/simplemoc/settings.py
@@ -57,10 +57,21 @@ WSGI_APPLICATION = 'simplemoc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME','simplemoc'),
+        'USER': os.environ.get('DB_USER','cezar'),
+        'PASSWORD': os.environ.get('DB_PASS','a17m10t55'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
