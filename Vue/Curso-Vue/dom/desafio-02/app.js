61c53899a2a457ec849815238aa09cee244ae0b1
cezar.nk <cezar.nk@gmail.com>
2019-05-14 21:38:51 -0300

    incluido mais arquivos da aula de VueJS

--- Vue/Curso-Vue/dom/desafio-02/app.js
+++ Vue/Curso-Vue/dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+      exibirAlerta() {
+        alert("Desafio alerta!")
+      },
+      alterarValor(event) {
+        this.valor = event.target.value
+      }
     }
 })
